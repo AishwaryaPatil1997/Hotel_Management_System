24c311e03dd4f5e4608a4e4350ef3c908dd2b146
Udaykumar <uday7shroff@gmail.com>
2019-12-22 19:35:00 +0530

    Updated Files

--- Angular/src/app/addroom/addroom.component.ts
+++ Angular/src/app/addroom/addroom.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-addroom',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddroomComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  addRoom() {
+    this.router.navigateByUrl('/');
+  }
 
   ngOnInit() {
   }
