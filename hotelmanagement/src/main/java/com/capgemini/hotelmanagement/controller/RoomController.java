938b6c8e84f050a740ba31b1b0f48a5a4ef38942
Sneha Jadhav <snehajadhav1511@gmail.com>
2019-12-23 19:01:18 +0530

    HBDM 1.4

--- hotelmanagement/src/main/java/com/capgemini/hotelmanagement/controller/RoomController.java
+++ hotelmanagement/src/main/java/com/capgemini/hotelmanagement/controller/RoomController.java
@@ -13,22 +13,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.hotelmanagement.beans.BookingInfoBean;
+import com.capgemini.hotelmanagement.beans.HistoryBean;
 import com.capgemini.hotelmanagement.beans.HotelResponse;
 import com.capgemini.hotelmanagement.beans.RoomBean;
-import com.capgemini.hotelmanagement.service.RoomServices;
+import com.capgemini.hotelmanagement.service.RoomService;
 
 @RestController
-//To connect rest with angular
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class RoomController {
 	@Autowired
-	private RoomServices userServices;
+	private RoomService roomService;
 
 	@PostMapping(path = "/bookingRoom", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public HotelResponse userRegistration(@RequestBody BookingInfoBean bookingInfoBean) {
 		HotelResponse hotelResponse = new HotelResponse();
-		if (userServices.checkRoomStatus(bookingInfoBean.getRoomId())) {
-			boolean isBooked = userServices.bookHotelRoom(bookingInfoBean);
+		if (roomService.checkRoomStatus(bookingInfoBean.getRoomId())) {
+			boolean isBooked = roomService.bookHotelRoom(bookingInfoBean);
 			if (isBooked) {
 				hotelResponse.setStatusCode(201);
 				hotelResponse.setMessage("Success");
@@ -48,7 +48,7 @@ public class RoomController {
 
 	@GetMapping(path = "/getAvailableRooms", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public HotelResponse showRooms(@RequestParam String roomType) {
-		List<RoomBean> roomList = userServices.showAvailableRoom(roomType);
+		List<RoomBean> roomList = roomService.showAvailableRoom(roomType);
 		HotelResponse hotelResponse = new HotelResponse();
 		if (roomList != null) {
 			hotelResponse.setStatusCode(201);
@@ -65,7 +65,7 @@ public class RoomController {
 
 	@GetMapping(path = "/getBookedRecords")
 	public HotelResponse showBookingDetails(@RequestParam int userId) {
-		List<BookingInfoBean> bookingInfoList = userServices.showBooking(userId);
+		List<BookingInfoBean> bookingInfoList = roomService.showBooking(userId);
 		HotelResponse hotelResponse = new HotelResponse();
 		if (bookingInfoList != null) {
 			hotelResponse.setStatusCode(201);
@@ -82,7 +82,7 @@ public class RoomController {
 
 	@DeleteMapping(path = "/cancelBooking")
 	public HotelResponse cancelBooking(@RequestParam int bookingId) {
-		boolean isCancel = userServices.cancelBooking(bookingId);
+		boolean isCancel = roomService.cancelBooking(bookingId);
 		HotelResponse hotelResponse = new HotelResponse();
 		if (isCancel) {
 			hotelResponse.setStatusCode(201);
@@ -98,7 +98,7 @@ public class RoomController {
 
 	@GetMapping(path = "/totalAmount")
 	public HotelResponse totalBill(@RequestParam int userId) {
-		double totalBill = userServices.totalBill(userId);
+		double totalBill = roomService.totalBill(userId);
 		HotelResponse hotelResponse = new HotelResponse();
 		if (totalBill > 0.0) {
 			hotelResponse.setStatusCode(201);
@@ -112,5 +112,74 @@ public class RoomController {
 		}
 		return hotelResponse;
 	}// End of totalBill()
+
+	@GetMapping(path = "/showHistory")
+	public HotelResponse showHistory(@RequestParam int userId) {
+		List<HistoryBean> historyList = roomService.showHistory(userId);
+		HotelResponse hotelResponse = new HotelResponse();
+		if (historyList != null) {
+			hotelResponse.setStatusCode(201);
+			hotelResponse.setMessage("Success");
+			hotelResponse.setHistoryList(historyList);
+			hotelResponse.setDescription("History Retrive Successfully.......");
+		} else {
+			hotelResponse.setStatusCode(401);
+			hotelResponse.setMessage("Failed");
+			hotelResponse.setDescription("Unable To Retrive History........");
+		}
+		return hotelResponse;
+	}// End of showHistory()
+
+	@GetMapping(path = "/getAllRooms", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public HotelResponse showAllRooms() {
+		List<RoomBean> roomList = roomService.showAllRooms();
+		HotelResponse hotelResponse = new HotelResponse();
+		if (roomList != null) {
+			hotelResponse.setStatusCode(201);
+			hotelResponse.setMessage("Success");
+			hotelResponse.setRoomList(roomList);
+			hotelResponse.setDescription("Retrieve All Rooms.......");
+		} else {
+			hotelResponse.setStatusCode(401);
+			hotelResponse.setMessage("Failed");
+			hotelResponse.setDescription("No Room Records Found........");
+		}
+		return hotelResponse;
+	}// End of showAllRooms()
+
+	@PostMapping(path = "/addNewRoom", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public HotelResponse addNewRoom(@RequestBody RoomBean roomBean) {
+		HotelResponse hotelResponse = new HotelResponse();
+
+		boolean isRoomAdded = roomService.addNewRoom(roomBean);
+		if (isRoomAdded) {
+			hotelResponse.setStatusCode(201);
+			hotelResponse.setMessage("Success");
+			hotelResponse.setDescription("New Room Added Successfully.......");
+		} else {
+			hotelResponse.setStatusCode(401);
+			hotelResponse.setMessage("Failed");
+			hotelResponse.setDescription("Failed To Add New Room........");
+		}
+		return hotelResponse;
+	}// End of addNewRoom()
+	
+	@DeleteMapping(path = "/deleteRoom")
+	public HotelResponse deleteRoom(int roomId) {
+
+		boolean isDeleted = roomService.deleteRoom(roomId);
+		HotelResponse hotelResponse = new HotelResponse();
+
+		if (isDeleted) {
+			hotelResponse.setStatusCode(201);
+			hotelResponse.setMessage("Success");
+			hotelResponse.setDescription("Room Record Deleted.....");
+		} else {
+			hotelResponse.setStatusCode(401);
+			hotelResponse.setMessage("Failed");
+			hotelResponse.setDescription("Enable to Delete Room Record.....");
+		}
+		return hotelResponse;
+	}// End of deleteRoom()
 
 }// End of Class
